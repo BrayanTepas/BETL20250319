523336e45a946d6bf4742d6c10eed160aba30b7c
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-03-19 10:46:12 -0600

    cambiar a español en model

--- BETL20250319.AppWebMVC/Models/Category.cs
+++ BETL20250319.AppWebMVC/Models/Category.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BETL20250319.AppWebMVC.Models;
 
 public partial class Category
 {
     public int CategoryId { get; set; }
-
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [Display(Name = "categoria")]
     public string CategoryName { get; set; } = null!;
-
+    [Display(Name = "descripcion")]
     public string? Description { get; set; }
 
+    
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
